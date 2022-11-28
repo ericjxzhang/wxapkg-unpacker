be44acd0fe00109ae39df7d6a6f04fc978eba312
ericjxzhang <89523553+ericjxzhang@users.noreply.github.com>
2022-11-28 15:41:42 +0800

    Update wuWxss.js
    Fix issues according to https://github.com/xuedingmiaojun/wxappUnpacker/issues/30#issuecomment-1066458529

--- wuWxss.js
+++ wuWxss.js
@@ -28,8 +28,14 @@ function doWxss(dir, cb, mainDir, nowDir) {
 
         function statistic(data) {
             function addStat(id) {
-                if (!importCnt[id]) importCnt[id] = 1, statistic(pureData[id]);
-				else ++importCnt[id];
+                if (!importCnt[id]) {
+					if (pureData) {
+						importCnt[id] = 1;
+						statistic(pureData[id]);
+					}
+				} else {
+					++importCnt[id];
+				}
 			}
 
             if (typeof data === "number") return addStat(data);
@@ -240,7 +246,7 @@ function doWxss(dir, cb, mainDir, nowDir) {
 
             code = code.slice(0, code.indexOf('\n'));
             let vm = new VM({sandbox: {}});
-            pureData = vm.run(code + "\n_C");
+            pureData = vm.run(code + "}");
 
 			console.log("Guess wxss(first turn)...");
             preRun(dir, frameFile, mainCode, files, () => {
